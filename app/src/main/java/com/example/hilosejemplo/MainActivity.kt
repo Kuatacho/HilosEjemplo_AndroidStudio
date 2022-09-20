2e07d5eb1ea6ec925f86fb81516ed11888fc1b26
Victor Jesus Chavez Alvarez <cav2024930@est.univalle.edu>
2022-09-20 09:06:20 -0400

    Conceptos hilos

--- app/src/main/java/com/example/hilosejemplo/MainActivity.kt
+++ app/src/main/java/com/example/hilosejemplo/MainActivity.kt
@@ -16,12 +16,42 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         movie=Movie("La Sirenita",4)
-        binding.btnIniciar.setOnClickListener{ iniciarProceso()}
+        binding.btnIniciar.setOnClickListener{
+            iniciarHilo()
+            iniciarProceso()}
     }
 
     private fun iniciarProceso() {
         binding.txtDescarga.text="Iniciando Descarga"
+        //Thread.sleep(5000)
         binding.txtDescarga.text="Finalizando Descarga"
+        binding.txtEjecucion.text=movie.play()
+    }
+    private fun iniciarHilo(){
+        /*Al configurar un hilo debemos implementar una interfaz
+        * denominada Runnable (Comando de instrucciones)
+        * a ejecutar... en esa interfaz existe una regla del juego que se
+        * llama run() todo lo que coloquemos dentro de run es lo que se
+        * ejecutara en segundo plano*/
+        binding.txtDescarga.text="Iniciando Descarga"
+        val hilo=Thread(Runnable {
+            try{
+                /*Estamos implementando directamente el run()*/
+                Thread.sleep(8000)
+                //Para hablar con la interfaz de usuario UI desde un hilo
+                //tenemos que crear un canal de comunicacion
+                runOnUiThread{binding.txtDescarga.text="Finalizando La Descarga"
+                }
+
+            }catch (e:InterruptedException){
+                e.printStackTrace()
+            }
+
+
+        })
+        /*Para que el hilo arranque y ejecute todo lo que esta
+        * en run () tienen queutilizar el metodo start()*/
+        hilo.start()
     }
 }
 
