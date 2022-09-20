8ac0764a58931d579f676af0f48fd9ffd32166e9
Victor Jesus Chavez Alvarez <cav2024930@est.univalle.edu>
2022-09-20 08:31:13 -0400

    a gradle/Module para activar el binding  inicializar vista de forma mas optima

--- app/src/main/java/com/example/hilosejemplo/MainActivity.kt
+++ app/src/main/java/com/example/hilosejemplo/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.hilosejemplo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hilosejemplo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /*ir a gradle/Module para activar el binding
+    * e inicializar vista de forma mas optima*/
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var movie:Movie
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        movie=Movie("La Sirenita",4)
+        binding.btnIniciar.setOnClickListener{ iniciarProceso()}
+    }
+
+    private fun iniciarProceso() {
+        binding.txtDescarga.text="Iniciando Descarga"
+        binding.txtDescarga.text="Finalizando Descarga"
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/hilosejemplo/Movie.kt
@@ -0,0 +1,14 @@
+package com.example.hilosejemplo
+
+class Movie(
+    private val name:String,
+    val duration :Int,
+
+) {
+    fun play():String{
+        var mensaje=""
+        for (i in 0 .. duration)
+            mensaje+="Playing movie: $name\n"
+        return mensaje
+    }
+}
